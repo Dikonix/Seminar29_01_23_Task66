7b13d9b2a6b98b44259681cddd47338f8be6c41f
Diana Brik <di.brik@yandex.kz>
2023-02-03 21:10:44 +0600

    Seminar 29.01.23 Task 66

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int SumNumbers(int from, int to)
+{
+    if (from == to)
+    {
+        return from;
+    }
+    var direction = to > from ? 1 : -1;
+    return SumNumbers(from + direction, to) + from;
+}
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumNumbers(firstNumber, secondNumber);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
